48c19575dd2c04aa60423ff5272ba16ffb910faa
liejiayong <809206619@qq.com>
2022-03-09 19:12:14 +0800

    feat:  Lazy rendering and Raven monitor

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,35 @@
-import React, { Component, Suspense } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import Home from '@/views/Home/index'
-import Test from '@/views/Test/index'
+import React, { Component, Suspense, lazy } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Loading from '@/components/Loading';
+import ErrorBoundary from '@/components/ErrorBoundary';
+
+// lazy load
+const lazyLoad = (path) =>
+  lazy(() => {
+    return new Promise((resolve) => setTimeout(resolve, 1 * 1000))
+      .then(() => import(`@/views/${path}`))
+      .catch(() => import('@/components/Error'));
+  });
+
+const Home = lazyLoad('Home');
+const Test = lazyLoad('Test');
 
 // @withRouter
 class Routes extends Component {
   render() {
     return (
-      <Suspense fallback={<p>loading...</p>}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/test" component={Test} />
+      <ErrorBoundary>
+        <Suspense fallback={<Loading />}>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/test' component={Test} />
 
-          <Route render={() => <h1>404</h1>} />
-        </Switch>
-      </Suspense>
-    )
+            <Route render={() => <h1>404</h1>} />
+          </Switch>
+        </Suspense>
+      </ErrorBoundary>
+    );
   }
 }
 
-export default Routes
+export default Routes;
